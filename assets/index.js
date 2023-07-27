c5fe76a6bb0fe2048bc5b8a2bbbdbb60ad2ed2fb
Nii Odoi <niiodoi15@yahoo.com>
2023-07-27 13:38:36 +0000

    question 2

--- assets/index.js
+++ assets/index.js
@@ -1,49 +1,40 @@
-const form = document.getElementById('form');
-let submitBtn = document.querySelector('.submit-btn');
-
 class Form {
-    constructor(firstname, lastname, age, country, picture) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.age = age;
-        this.country = country;
-        this.picture = picture;
-    }
-
     async validate() {
+      const firstname = document.querySelector('.firstname').value;
+      const lastname = document.querySelector('.lastname').value;
+      const age = document.querySelector('.age').value;
+      const country = document.querySelector('.country').value;
+      const picture = document.querySelector('.picture').value;
 
-        let firstname = document.querySelector('.firstname').value;
-        let lastname = document.querySelector('.lastname').value;
-        let age = document.querySelector('.age').value;
-        let country = document.querySelector('.country').value;
-        let picture = document.querySelector('.picture').value;
+      if (firstname === '' || lastname === '' || age === '' || country === '' || picture === '') {
+        alert('Fields are empty');
+        return false; // Return false if the form is invalid
+      }
 
-        if (firstname === '' || lastname === '' || age === '' || country === '' || picture === '') {
-            alert('Fields are empty')
-        }
+      return true; // Return true if the form is valid
     }
 
     sentence() {
+      const firstname = document.querySelector('.firstname').value;
+      const lastname = document.querySelector('.lastname').value;
+      const age = document.querySelector('.age').value;
+      const country = document.querySelector('.country').value;
+      const picture = document.querySelector('.picture').value;
 
-        let firstname = document.querySelector('.firstname').value;
-        let lastname = document.querySelector('.lastname').value;
-        let age = document.querySelector('.age').value;
-        let country = document.querySelector('.country').value;
-        let picture = document.querySelector('.picture').value;
-
-        alert(`My name is ${firstname} ${lastname}. I am from ${country}. I am ${age} years old`);
+      alert(`My name is ${firstname} ${lastname}. I am from ${country}. I am ${age} years old`);
     }
-}
+  }
 
-const newForms = new Form(firstname, lastname, age, country, picture);
+  const submitBtn = document.querySelector('.submit-btn');
+  const newForm = new Form();
 
-submitBtn.addEventListener('click', async function (event) {
-    event.preventDefault;
-    const isValid = await Form.validate();
+  submitBtn.addEventListener('click', async function (event) {
+    event.preventDefault();
+    const isValid = await newForm.validate();
 
     if (isValid) {
-        return newForms.sentence();
+      newForm.sentence();
     } else {
-        alert('Please fill in all the required fields.');
+      alert('Please fill in all the required fields.');
     }
-});
+  });
--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,12 +1,12 @@
 function fizzBuzz(n) {
     let fizzArray = [];
     for (let i = 1; i <= n; i++) {
-      if (i % 3 === 0) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        fizzArray.push('FizzBuzz');
+      } else if (i % 3 === 0) {
         fizzArray.push('Fizz');
       } else if (i % 5 === 0) {
         fizzArray.push('Buzz');
-      } else if (i % 3 === 0 && i % 5 === 0) {
-        fizzArray.push('FizzBuzz');
       } else {
         fizzArray.push(i.toString());
       }
